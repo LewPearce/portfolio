18683442460681b1d1a6b3a935a1b880eb87e3de
LewPearce <lew@DESKTOP-0NCRVJR>
2023-05-08 13:25:49 +0100

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,4 @@
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-
 const Footer = () => {
-  const path = useLocation().pathname;
-
   return (
     <>
       <div className="footer"></div>
